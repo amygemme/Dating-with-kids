d1e670bcdaa16370c2534c2de6a15a41dd0084e4
amygemme81 <domathlikearockstar@gmail.com>
2019-04-11 13:03:36 -0700

    adding back the index.js adn js folder

---
+++ public/js/profile.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $.get("/api/dating")
+    // On success, run the following code
+    .then(function (data) {
+      console.log(data, "data");
+      $("#profile-area").prepend(data);
+
+      console.log("end of code, should display and add")
+
+    });
+
+
+})
